4421416b360de2cd703cf2f3a95b03cd7a4fa339
heguoling <2319100642@qq.com>
2024-05-25 19:37:13 +0800

    add gtest

---
+++ src/TestHashGirid.h
@@ -0,0 +1,32 @@
+#pragma once
+
+using namespace MathLib;
+class TestHashGirid:public testing::Test
+{
+public:
+	TestHashGirid()
+	{
+		// initialization code here
+	}
+
+	~TestHashGirid()
+	{
+		// cleanup any pending stuff, but no exceptions allowed
+	}
+
+	void SetUp() override
+	{
+		// code here will execute just before the test ensues
+	}
+
+	void TearDown() override
+	{
+		// code here will be called just after the test completes
+		// ok to through exceptions from here if need be
+	}	
+};
+
+TEST_F(TestHashGirid, TestHashGrid)
+{
+	// Test code here
+}
